f3c00912d51c657582a8b48882b2abf378bfc97f
denyAndriana0107 <denyandriana06@gmail.com>
2020-10-12 15:57:22 +0700

    First commit

---
+++ app/Views/pages/home.php
@@ -0,0 +1,12 @@
+<?=$this->extend('layout/template');?>
+<?=$this->section('content');?>  
+<div class="container">
+    <div class="ro">
+        <div class="col">
+            <h3>Hello World !</h3>
+        </div>
+    </div>
+</div>
+<?=$this->endSection();?>
+
+
